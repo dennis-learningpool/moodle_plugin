42c2b024a7df22837b6b9c234fceeab9deb1352a
Dennis Heaney <dennis@learningpool.com>
2021-07-22 10:55:11 +0100

    Update file headers to standard GPLv3 Moodle headers

--- local/kaltura/API/KalturaTypes.php
+++ local/kaltura/API/KalturaTypes.php
@@ -1,31 +1,23 @@
 <?php
-// ===================================================================================================
-//                           _  __     _ _
-//                          | |/ /__ _| | |_ _  _ _ _ __ _
-//                          | ' </ _` | |  _| || | '_/ _` |
-//                          |_|\_\__,_|_|\__|\_,_|_| \__,_|
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
-// This file is part of the Kaltura Collaborative Media Suite which allows users
-// to do with audio, video, and animation what Wiki platfroms allow them to do with
-// text.
-//
-// Copyright (C) 2006-2018  Kaltura Inc.
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
+// Moodle is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// @ignore
-// ===================================================================================================
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    local_kaltura
+ * @author     Remote-Learner.net Inc
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
+ * @copyright  (C) 2014 Remote Learner.net Inc http://www.remote-learner.net
+ */
 
 /**
  * @package Kaltura
